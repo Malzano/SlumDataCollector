63b0aef36b4b55ad0b5a55bd8c415457fc7b277c
Malzano <42615256+Malzano@users.noreply.github.com>
2020-11-19 01:02:48 +0700

    Add files via upload

---
+++ slumProj/slumProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slumProj
+//
+//  Created by อธิคม ปิยะบงการ on 11/19/2563 BE.
+//  Copyright © 2563 BE อธิคม ปิยะบงการ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
